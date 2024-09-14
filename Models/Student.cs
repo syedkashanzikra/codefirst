4dbafb3034d269ea9100e88335a8965671e2d40a
SyedKashanAbbasNaqvi <kashan@zikrainfotech.com>
2024-09-15 00:54:29 +0500

    First Commit

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApproach.Models
+{
+    public class Student
+    {
+        [Key]
+        [Column("Student_ID")]
+        public int Id { get; set; }
+
+        [Column("StudentName",TypeName = "varchar(60)")]
+        public string Name { get; set; }
+
+        [Column("StudentGender", TypeName = "varchar(7)")]
+        public string Gender { get; set; }
+
+        [Column("Student_Age")]
+        public int Age { get; set; }
+        
+
+    }
+}
